07da9c22d4a0a7f8a7b6911e9a9abab782ee5a92
katpro13 <katpro13@gmail.com>
2019-05-20 15:26:07 -0700

    k

---
+++ code1/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(800, 600, WEBGL);
+}
+function draw(){
+
+  if(mouseIsPressed){
+    fill(255,207,17);
+  } else {
+    fill(73,135,76);
+  }
+  
+  ellipse(mouseX,mouseY,80,80);
+  if (keyIsPressed === true) {
+    fill(27,132,50);
+  }else {
+    fill(25,0,51);
+  }
+ rect(25,9,25,51);
+  fill(45,173,45)
+  sphere(50);
+}
---
+++ slotmc.js
@@ -0,0 +1,1 @@
+odoo.default({ el:'.js-odoo', from: '60% of immigrants said they immigrated due to economkc disaprity', to: '20% of immigrants said they immigrated for better oppurtunities', animationDelay: 1000 });
